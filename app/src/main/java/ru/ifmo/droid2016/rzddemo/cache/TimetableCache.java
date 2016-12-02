163efb49f1766034886ddc39f83c2bd34ea32994
karavaevitalii <fkve97@gmail.com>
2016-12-02 11:43:00 +0300

    cache doses't work

--- app/src/main/java/ru/ifmo/droid2016/rzddemo/cache/TimetableCache.java
+++ app/src/main/java/ru/ifmo/droid2016/rzddemo/cache/TimetableCache.java
@@ -1,25 +1,32 @@
 package ru.ifmo.droid2016.rzddemo.cache;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.support.annotation.AnyThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.WorkerThread;
 
 import java.io.FileNotFoundException;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
+import ru.ifmo.droid2016.rzddemo.db.TimetableDBHelper;
 import ru.ifmo.droid2016.rzddemo.model.TimetableEntry;
+import ru.ifmo.droid2016.rzddemo.utils.TimeUtils;
 
 import static ru.ifmo.droid2016.rzddemo.Constants.LOG_DATE_FORMAT;
+import static ru.ifmo.droid2016.rzddemo.db.TimetableContract.Timetable.*;
 
 /**
  * Кэш расписания поездов.
- *
+ * <p>
  * Ключом является комбинация трех значений:
  * ID станции отправления, ID станции прибытия, дата в москомском часовом поясе
- *
+ * <p>
  * Единицей хранения является список поездов - {@link TimetableEntry}.
  */
 
@@ -36,7 +43,7 @@ public class TimetableCache {
 
     /**
      * Создает экземпляр кэша с указанной версией модели данных.
-     *
+     * <p>
      * Может вызываться на лююбом (в том числе UI потоке). Может быть создано несколько инстансов
      * {@link TimetableCache} -- все они должны потокобезопасно работать с одним физическим кэшом.
      */
@@ -54,9 +61,7 @@ public class TimetableCache {
      * @param fromStationId ID станции отправления
      * @param toStationId   ID станции прибытия
      * @param dateMsk       дата в московском часовом поясе
-     *
      * @return - список {@link TimetableEntry}
-     *
      * @throws FileNotFoundException - если в кэше отсуствуют запрашиваемые данные.
      */
     @WorkerThread
@@ -65,17 +70,147 @@ public class TimetableCache {
                                     @NonNull String toStationId,
                                     @NonNull Calendar dateMsk)
             throws FileNotFoundException {
-        // TODO: ДЗ - реализовать кэш на основе базы данных SQLite с учетом версии модели данных
-        throw new FileNotFoundException("No data in timetable cache for: fromStationId="
-                + fromStationId + ", toStationId=" + toStationId
-                + ", dateMsk=" + LOG_DATE_FORMAT.format(dateMsk.getTime()));
+        SQLiteDatabase db = TimetableDBHelper.getInstance(context, version).getReadableDatabase();
+
+        String[] columns = (version == DataSchemeVersion.V1
+                ?
+                new String[]{DEPARTURE_STATION_ID
+                        , DEPARTURE_STATION_NAME
+                        , DEPARTURE_TIME
+                        , ARRIVAL_STATION_ID
+                        , ARRIVAL_STATION_NAME
+                        , ARRIVAL_TIME
+                        , TRAIN_ROUTE_ID
+                        , ROUTE_START_STATION_NAME
+                        , ROUTE_END_STATION_NAME}
+                :
+                new String[]{DEPARTURE_STATION_ID
+                        , DEPARTURE_STATION_NAME
+                        , DEPARTURE_TIME
+                        , ARRIVAL_STATION_ID
+                        , ARRIVAL_STATION_NAME
+                        , ARRIVAL_TIME
+                        , TRAIN_ROUTE_ID
+                        , ROUTE_START_STATION_NAME
+                        , ROUTE_END_STATION_NAME
+                        , TRAIN_NAME});
+
+        String projection = DEPARTURE_DATE + "=? AND "
+                + DEPARTURE_STATION_ID + "=? AND "
+                + ARRIVAL_STATION_ID + "=?";
+
+        String[] selectionArgs = new String[]{fromStationId
+                , toStationId, Long.toString(getDate(dateMsk))};
+
+        Cursor c = null;
+        try {
+            c = db.query(TABLE, columns, projection, selectionArgs, null, null, null);
+
+            if (c != null && c.moveToFirst()) {
+
+                List<TimetableEntry> ret = new ArrayList<>();
+
+                for (; c.isAfterLast(); c.moveToNext()) {
+                    int i = 0;
+                    String departureStationId = c.getString(i++);
+                    String departureStationName = c.getString(i++);
+                    Calendar departureTime = getCurIime(c.getLong(i++));
+                    String arrivalStationId = c.getString(i++);
+                    String arrivalStationName = c.getString(i++);
+                    Calendar arrivalTime = getCurIime(c.getLong(i++));
+                    String trainRouteId = c.getString(i++);
+                    String routeStartStationName = c.getString(i++);
+                    String routeEndStationName = c.getString(i++);
+                    String trainName = (version == DataSchemeVersion.V1
+                            ? null
+                            : c.getString(i));
+
+                    ret.add(new TimetableEntry(departureStationId, departureStationName
+                            , departureTime, arrivalStationId, arrivalStationName, arrivalTime
+                            , trainRouteId, trainName, routeStartStationName, routeEndStationName));
+                }
+
+                return ret;
+            } else {
+                throw new FileNotFoundException("No data in timetable cache for: fromStationId="
+                        + fromStationId + ", toStationId=" + toStationId
+                        + ", dateMsk=" + LOG_DATE_FORMAT.format(dateMsk.getTime()));
+            }
+        } finally {
+            if (c != null) {
+                c.close();
+            }
+
+            db.close();
+        }
+    }
+
+    private Calendar getCurIime(long aLong) {
+        Calendar c = Calendar.getInstance(TimeUtils.getMskTimeZone());
+        c.setTime(new Date(aLong));
+        return c;
+    }
+
+    private long getDate(Calendar dateMSK) {
+        return dateMSK.get(Calendar.DAY_OF_YEAR) + dateMSK.get(Calendar.YEAR) * 500;
     }
 
     @WorkerThread
-    public void put(@NonNull String fromStationId,
-                    @NonNull String toStationId,
-                    @NonNull Calendar dateMsk,
+    public void put(@NonNull Calendar dateMsk,
                     @NonNull List<TimetableEntry> timetable) {
-        // TODO: ДЗ - реализовать кэш на основе базы данных SQLite с учетом версии модели данных
+        SQLiteDatabase db = TimetableDBHelper.getInstance(context, version).getWritableDatabase();
+
+        SQLiteStatement insert = null;
+        db.beginTransaction();
+        try {
+            insert = db.compileStatement("INSERT INTO " + TABLE + " ("
+                    + DEPARTURE_DATE + ", "
+                    + DEPARTURE_STATION_ID + ", "
+                    + DEPARTURE_STATION_NAME + ", "
+                    + DEPARTURE_TIME + ", "
+                    + ARRIVAL_STATION_ID + ", "
+                    + ARRIVAL_STATION_NAME + ", "
+                    + ARRIVAL_TIME + ", "
+                    + TRAIN_ROUTE_ID + ", "
+                    + ROUTE_START_STATION_NAME + ", "
+                    + ROUTE_END_STATION_NAME
+                    + (version == DataSchemeVersion.V1
+                    ? ") VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                    : ", " + TRAIN_NAME + ") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"));
+
+            for (TimetableEntry entry : timetable) {
+                int i = 0;
+                insert.bindLong(++i, getDate(dateMsk));
+                insert.bindString(++i, entry.departureStationId);
+                insert.bindString(++i, entry.departureStationName);
+                insert.bindLong(++i, entry.departureTime.getTimeInMillis());
+                insert.bindString(++i, entry.arrivalStationId);
+                insert.bindString(++i, entry.arrivalStationName);
+                insert.bindLong(++i, entry.arrivalTime.getTimeInMillis());
+                insert.bindString(++i, entry.trainRouteId);
+                insert.bindString(++i, entry.routeStartStationName);
+                insert.bindString(++i, entry.routeEndStationName);
+                if (version == DataSchemeVersion.V2) {
+                    if (entry.trainName != null) {
+                        insert.bindString(++i, entry.trainName);
+                    } else {
+                        insert.bindNull(++i);
+                    }
+                }
+
+                insert.executeInsert();
+            }
+            db.setTransactionSuccessful();
+        } finally {
+            if (insert != null) {
+                try {
+                    insert.close();
+                } catch (Exception ignored) {
+                }
+            }
+            db.endTransaction();
+        }
+
+        db.close();
     }
 }
---
+++ app/src/main/java/ru/ifmo/droid2016/rzddemo/db/TimetableContract.java
@@ -0,0 +1,56 @@
+package ru.ifmo.droid2016.rzddemo.db;
+
+public class TimetableContract {
+
+    public interface TimetableColumns {
+
+        String TIMETABLE_ID = "_id";
+
+        String DEPARTURE_DATE = "departureDate";
+
+        String DEPARTURE_STATION_ID = "departureStationId";
+
+        String DEPARTURE_STATION_NAME = "departureStationName";
+
+        String DEPARTURE_TIME = "departureTime";
+
+        String ARRIVAL_STATION_ID = "arrivalStationId";
+
+        String ARRIVAL_STATION_NAME = "arrivalStationName";
+
+        String ARRIVAL_TIME = "arrivalTime";
+
+        String TRAIN_ROUTE_ID = "trainRouteId";
+
+        String TRAIN_NAME = "trainName";
+
+        String ROUTE_START_STATION_NAME = "routeStartStationName";
+
+        String ROUTE_END_STATION_NAME = "routeEndStationName";
+    }
+
+    public static final class Timetable implements TimetableColumns {
+
+        public static final String TABLE = "timetable";
+
+        private static final String CREATE_TABLE = "CREATE TABLE " + TABLE
+                + " ("
+                + TIMETABLE_ID + " INTEGER PRIMARY KEY, "
+                + DEPARTURE_DATE + " INTEGER, "
+                + DEPARTURE_STATION_ID + " TEXT, "
+                + DEPARTURE_STATION_NAME + " TEXT, "
+                + DEPARTURE_TIME + " INTEGER, "
+                + ARRIVAL_STATION_ID + " TEXT, "
+                + ARRIVAL_STATION_NAME + " TEXT, "
+                + ARRIVAL_TIME + " INTEGER, "
+                + TRAIN_ROUTE_ID + " TEXT, "
+                + ROUTE_START_STATION_NAME + " TEXT, "
+                + ROUTE_END_STATION_NAME + " TEXT";
+
+        public static final String CREATE_TABLE_V1 = CREATE_TABLE
+                + " )";
+
+        public static final String CREATE_TABLE_V2 = CREATE_TABLE
+                + ", " + TRAIN_NAME + " TEXT )";
+    }
+}
