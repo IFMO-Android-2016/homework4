d0299da5c21e467e9411854b4b46dc65861ae2ec
MikhailovNikita <mikhailovnikitau@gmail.com>
2016-12-08 23:54:02 +0300

    Fixed?

--- app/src/main/java/ru/ifmo/droid2016/rzddemo/cache/TimetableCache.java
+++ app/src/main/java/ru/ifmo/droid2016/rzddemo/cache/TimetableCache.java
@@ -5,7 +5,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteStatement;
-import android.provider.ContactsContract;
 import android.support.annotation.AnyThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.WorkerThread;
@@ -33,20 +32,21 @@ import static ru.ifmo.droid2016.rzddemo.Constants.LOG_DATE_FORMAT;
  */
 
 public class TimetableCache {
-
     @NonNull
     private final Context context;
 
-
-    private final TrainScheduleDatabaseHandler dbHandler;
-    private final SimpleDateFormat dateFormat;
-    private final SimpleDateFormat dateFormatWithTime;
-
     /**
-     * Версия модели данных, с которой работает кэщ.
+     * Версия модели данных, с которой работает кэш.
      */
     @DataSchemeVersion
     private final int version;
+
+    /**
+     * Database handler with methods for timetable table
+     */
+    private final TimetableDatabaseHandler dbHandler;
+    private final SimpleDateFormat dateFormat;
+    private final SimpleDateFormat dateFormatWithTime;
 
     /**
      * Создает экземпляр кэша с указанной версией модели данных.
@@ -60,7 +60,7 @@ public class TimetableCache {
         this.context = context.getApplicationContext();
         this.version = version;
 
-        this.dbHandler = new TrainScheduleDatabaseHandler(this.context, version);
+        this.dbHandler = new TimetableDatabaseHandler(this.context, version);
         this.dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
         this.dateFormatWithTime = new SimpleDateFormat("dd-MM-yyyy'T'HH:mm", Locale.ENGLISH);
     }
@@ -82,14 +82,12 @@ public class TimetableCache {
                                     @NonNull Calendar dateMsk)
             throws FileNotFoundException {
 
-
-        // TODO: ДЗ - реализовать кэш на основе базы данных SQLite с учетом версии модели данных
-
         SQLiteDatabase sqLiteDatabase = dbHandler.getReadableDatabase();
 
         Cursor cursor = sqLiteDatabase.query(
                 TrainScheduleContract.TABLE_NAME,
-                ((version == DataSchemeVersion.V2) ?
+                ((version == DataSchemeVersion.V2)
+                        ?
                         new String[]{
                                 TrainScheduleContract.DEPARTURE_STATION_ID,
                                 TrainScheduleContract.DEPARTURE_STATION_NAME,
@@ -114,7 +112,8 @@ public class TimetableCache {
                                 TrainScheduleContract.ROUTE_START_STATION_NAME,
                                 TrainScheduleContract.ROUTE_END_STATION_NAME
                         }
-                ), TrainScheduleContract.DATE + "=? AND "
+                ),
+                TrainScheduleContract.DATE + "=? AND "
                         + TrainScheduleContract.DEPARTURE_STATION_ID + "=? AND "
                         + TrainScheduleContract.ARRIVAL_STATION_ID + "=?",
                 new String[]{
@@ -125,55 +124,54 @@ public class TimetableCache {
                 null,
                 null,
                 null,
-                null);
+                null
+        );
 
-
-        try{
-            if(cursor != null && cursor.moveToFirst()){
+        try {
+            if (cursor != null && cursor.moveToFirst()) {
                 List<TimetableEntry> schedule = new ArrayList<>();
 
-
-                do{
+                while(true){
                     TimetableEntry timetableEntry;
 
-                    try{
-                        int count = 0;
+                    try {
+                        int pos = 0;
 
                         timetableEntry = new TimetableEntry(
-                                cursor.getString(count++),
-                                cursor.getString(count++),
-                                parseCalendar(cursor.getString(count++)),
-                                cursor.getString(count++),
-                                cursor.getString(count++),
-                                parseCalendar(cursor.getString(count++)),
-                                cursor.getString(count++),
-                                ((version == DataSchemeVersion.V2) ? cursor.getString(count++) : null),
-                                cursor.getString(count++),
-                                cursor.getString(count)
+                                cursor.getString(pos++),
+                                cursor.getString(pos++),
+                                parseDate(cursor.getString(pos++)),
+                                cursor.getString(pos++),
+                                cursor.getString(pos++),
+                                parseDate(cursor.getString(pos++)),
+                                cursor.getString(pos++),
+                                ((version == DataSchemeVersion.V2) ? cursor.getString(pos++) : null),
+                                cursor.getString(pos++),
+                                cursor.getString(pos)
                         );
-                    }catch (ParseException e){
+                    } catch (ParseException e) {
                         e.printStackTrace();
                         continue;
                     }
 
                     schedule.add(timetableEntry);
 
-                }while(cursor.moveToNext());
+                    if(!cursor.moveToNext()) break;
+                }
 
-            return schedule;
+                return schedule;
             }
-        }finally{
-            if(cursor != null){
+
+            throw new FileNotFoundException("No data in timetable cache for: fromStationId="
+                    + fromStationId + ", toStationId=" + toStationId
+                    + ", dateMsk=" + LOG_DATE_FORMAT.format(dateMsk.getTime()));
+        } finally {
+            if (cursor != null) {
                 cursor.close();
             }
 
             sqLiteDatabase.close();
         }
-
-
-        throw new FileNotFoundException("No data in timetable cache for: fromStationId="
-                + fromStationId + ", toStationId=" + toStationId
-                + ", dateMsk=" + LOG_DATE_FORMAT.format(dateMsk.getTime()));
     }
 
     @WorkerThread
@@ -181,8 +179,6 @@ public class TimetableCache {
                     @NonNull String toStationId,
                     @NonNull Calendar dateMsk,
                     @NonNull List<TimetableEntry> timetable) {
-        // TODO: ДЗ - реализовать кэш на основе базы данных SQLite с учетом версии модели данных
-
         SQLiteDatabase sqLiteDatabase = dbHandler.getWritableDatabase();
 
         String query = "INSERT INTO " + TrainScheduleContract.TABLE_NAME + " ("
@@ -195,117 +191,119 @@ public class TimetableCache {
                 + TrainScheduleContract.ARRIVAL_TIME + ","
                 + TrainScheduleContract.TRAIN_ROUTE_ID + ",";
 
-        if(version == DataSchemeVersion.V2)
-            query += TrainScheduleContract.TRAIN_NAME + ",";
+        if (version == DataSchemeVersion.V2) query += TrainScheduleContract.TRAIN_NAME + ",";
 
         query += TrainScheduleContract.ROUTE_START_STATION_NAME + ","
                 + TrainScheduleContract.ROUTE_END_STATION_NAME
                 + ") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?";
 
-        if(version == DataSchemeVersion.V2)
-            query += ", ?";
-
+        if(version == DataSchemeVersion.V2) query += ", ?";
 
         query += ")";
+
+
+
 
         SQLiteStatement statement = sqLiteDatabase.compileStatement(query);
 
         sqLiteDatabase.beginTransaction();
 
-        try{
-            for(TimetableEntry timetableEntry : timetable){
-                int count = 1;
+        try {
+            for (TimetableEntry timetableEntry : timetable) {
+                int pos = 1;
 
-                statement.bindString(count++, dateFormat.format(dateMsk.getTime()));
-                statement.bindString(count++, timetableEntry.departureStationId);
-                statement.bindString(count++, timetableEntry.departureStationName);
-                statement.bindString(count++, dateFormatWithTime.format(timetableEntry.departureTime.getTime()));
-                statement.bindString(count++, timetableEntry.arrivalStationId);
-                statement.bindString(count++, timetableEntry.arrivalStationName);
-                statement.bindString(count++, dateFormatWithTime.format(timetableEntry.arrivalTime.getTime()));
-                statement.bindString(count++, timetableEntry.trainRouteId);
+                statement.bindString(pos++, dateFormat.format(dateMsk.getTime()));
+                statement.bindString(pos++, timetableEntry.departureStationId);
+                statement.bindString(pos++, timetableEntry.departureStationName);
+                statement.bindString(pos++, dateFormatWithTime.format(timetableEntry.departureTime.getTime()));
+                statement.bindString(pos++, timetableEntry.arrivalStationId);
+                statement.bindString(pos++, timetableEntry.arrivalStationName);
+                statement.bindString(pos++, dateFormatWithTime.format(timetableEntry.arrivalTime.getTime()));
+                statement.bindString(pos++, timetableEntry.trainRouteId);
 
-                if (version == DataSchemeVersion.V2) {
-                    if (timetableEntry.trainName == null) {
-                       statement.bindNull(count++);
-                    } else {
-                        statement.bindString(count++, timetableEntry.trainName);
-                    }
-                }
 
-                statement.bindString(count++, timetableEntry.routeStartStationName);
-                statement.bindString(count++, timetableEntry.routeEndStationName);
+                if(version == DataSchemeVersion.V2 && timetableEntry.trainName == null)
+                    statement.bindNull(pos++);
+
+                if(version == DataSchemeVersion.V2 && timetableEntry.trainName != null)
+                    statement.bindString(pos++, timetableEntry.trainName);
+
+
+                statement.bindString(pos++, timetableEntry.routeStartStationName);
+                statement.bindString(pos++, timetableEntry.routeEndStationName);
 
                 statement.executeInsert();
             }
 
             sqLiteDatabase.setTransactionSuccessful();
-
-
-        }finally {
+        } finally {
             sqLiteDatabase.endTransaction();
         }
 
         sqLiteDatabase.close();
     }
 
-    private Calendar parseCalendar(String s) throws ParseException{
+    private Calendar parseDate(String date) throws ParseException {
         Calendar calendar = Calendar.getInstance();
+
+        calendar.setTime(dateFormatWithTime.parse(date));
         calendar.setTimeZone(TimeUtils.getMskTimeZone());
-        calendar.setTime(dateFormatWithTime.parse(s));
 
         return calendar;
     }
 
-    private static class TrainScheduleDatabaseHandler extends SQLiteOpenHelper {
-        private static final String DATABASE_NAME = "rzd.train_schedule.db";
+    private static class TimetableDatabaseHandler extends SQLiteOpenHelper {
+        private static final String DATABASE_NAME = "train_schedule_rzd";
 
         @DataSchemeVersion
         private int version;
 
-        public TrainScheduleDatabaseHandler(Context context, @DataSchemeVersion int version) {
+        TimetableDatabaseHandler(Context context, @DataSchemeVersion int version) {
             super(context, DATABASE_NAME, null, version);
             this.version = version;
         }
 
-        private void createTable(SQLiteDatabase sqLiteDatabase, @DataSchemeVersion int version) {
-            String query = "CREATE TABLE " + TrainScheduleContract.TABLE_NAME + " ("
-                    + TrainScheduleContract.ID + " INTEGER PRIMARY KEY,"
-                    + TrainScheduleContract.DATE + " TEXT,"
-                    + TrainScheduleContract.DEPARTURE_STATION_ID + " TEXT,"
-                    + TrainScheduleContract.DEPARTURE_STATION_NAME + " TEXT,"
-                    + TrainScheduleContract.DEPARTURE_TIME + " TEXT,"
-                    + TrainScheduleContract.ARRIVAL_STATION_ID + " TEXT,"
-                    + TrainScheduleContract.ARRIVAL_STATION_NAME + " TEXT,"
-                    + TrainScheduleContract.ARRIVAL_TIME + " TEXT,"
-                    + TrainScheduleContract.TRAIN_ROUTE_ID + " TEXT,";
-            if (version == DataSchemeVersion.V2)
-                query += TrainScheduleContract.TRAIN_NAME + " TEXT,";
-
-            query += TrainScheduleContract.ROUTE_START_STATION_NAME + " TEXT,"
-                    + TrainScheduleContract.ROUTE_END_STATION_NAME + "TEXT)";
-
-            sqLiteDatabase.execSQL(query);
-
+        private void createTable(SQLiteDatabase db, @DataSchemeVersion int version) {
+            db.execSQL(
+                    "CREATE TABLE " + TrainScheduleContract.TABLE_NAME + " ("
+                            + TrainScheduleContract.ID + " INTEGER PRIMARY KEY,"
+                            + TrainScheduleContract.DATE + " TEXT,"
+                            + TrainScheduleContract.DEPARTURE_STATION_ID + " TEXT,"
+                            + TrainScheduleContract.DEPARTURE_STATION_NAME + " TEXT,"
+                            + TrainScheduleContract.DEPARTURE_TIME + " TEXT,"
+                            + TrainScheduleContract.ARRIVAL_STATION_ID + " TEXT,"
+                            + TrainScheduleContract.ARRIVAL_STATION_NAME + " TEXT,"
+                            + TrainScheduleContract.ARRIVAL_TIME + " TEXT,"
+                            + TrainScheduleContract.TRAIN_ROUTE_ID + " TEXT,"
+                            + ((version == DataSchemeVersion.V2)
+                            ? TrainScheduleContract.TRAIN_NAME + " TEXT,": "")
+                            + TrainScheduleContract.ROUTE_START_STATION_NAME + " TEXT,"
+                            + TrainScheduleContract.ROUTE_END_STATION_NAME + " TEXT)"
+            );
         }
 
         @Override
-        public void onCreate(SQLiteDatabase sqLiteDatabase) {
-            createTable(sqLiteDatabase, version);
+        public void onCreate(SQLiteDatabase db) {
+            createTable(db, version);
         }
 
         @Override
-        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-            sqLiteDatabase.execSQL("ALTER TABLE " + TrainScheduleContract.TABLE_NAME + " "
-                    + "ADD COLUMN " + TrainScheduleContract.TRAIN_NAME + "TEXT");
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL(
+                    "ALTER TABLE " + TrainScheduleContract.TABLE_NAME + " "
+                            + "ADD COLUMN " + TrainScheduleContract.TRAIN_NAME + " TEXT");
         }
 
-        public void onDowngrade(SQLiteDatabase sqLite, int oldVersion, int newVersion) {
-            final String DOWNGRADE_TABLE_NAME = "downgrade_" + TrainScheduleContract.TABLE_NAME;
+        @Override
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            final String DOWNGRADE_TABLE_NAME = "downgraded_" + TrainScheduleContract.TABLE_NAME ;
 
-            sqLite.execSQL("ALTER TABLE " + TrainScheduleContract.TABLE_NAME + " " + "RENAME TO " + DOWNGRADE_TABLE_NAME);
+            db.execSQL(
+                    "ALTER TABLE " + TrainScheduleContract.TABLE_NAME + " " +
+                            "RENAME TO " + DOWNGRADE_TABLE_NAME
+            );
 
-            createTable(sqLite, DataSchemeVersion.V1);
+            createTable(db, DataSchemeVersion.V1);
 
             String columns = TrainScheduleContract.ID + ","
                     + TrainScheduleContract.DATE + ","
@@ -319,33 +317,13 @@ public class TimetableCache {
                     + TrainScheduleContract.ROUTE_START_STATION_NAME + ","
                     + TrainScheduleContract.ROUTE_END_STATION_NAME;
 
+            db.execSQL(
+                    "INSERT INTO " + TrainScheduleContract.TABLE_NAME + " (" + columns + ") " +
+                            "SELECT " + columns + " " +
+                            "FROM " + DOWNGRADE_TABLE_NAME
+            );
 
-            sqLite.execSQL("INSERT INTO " + TrainScheduleContract.TABLE_NAME + " (" + columns + ") " +
-                    "SELECT " + columns + " " + "FROM " + DOWNGRADE_TABLE_NAME);
-
-            sqLite.execSQL("DROP TABLE " + DOWNGRADE_TABLE_NAME);
-
+            db.execSQL("DROP TABLE " + DOWNGRADE_TABLE_NAME);
         }
-    }
-
-
-    private static final class TrainScheduleContract {
-
-
-        static final String ID = "id";
-        static final String DATE = "date";
-        static final String DEPARTURE_STATION_ID = "departure_station_id";
-        static final String DEPARTURE_STATION_NAME = "departure_station_name";
-        static final String DEPARTURE_TIME = "departure_time";
-        static final String ARRIVAL_STATION_ID = "arrival_station_id";
-        static final String ARRIVAL_STATION_NAME = "arrival_station_name";
-        static final String ARRIVAL_TIME = "arrival_time";
-        static final String TRAIN_ROUTE_ID = "train_route_id";
-        static final String TRAIN_NAME = "train_name";
-        static final String ROUTE_START_STATION_NAME = "route_start_station_name";
-        static final String ROUTE_END_STATION_NAME = "route_end_station_name";
-
-
-        static final String TABLE_NAME = "timetable";
     }
 }
---
+++ app/src/main/java/ru/ifmo/droid2016/rzddemo/cache/TrainScheduleContract.java
@@ -0,0 +1,27 @@
+package ru.ifmo.droid2016.rzddemo.cache;
+
+/**
+ * Created by Михайлов Никита on 07.12.2016.
+ * homework4
+ */
+
+public class TrainScheduleContract {
+
+    //Table database captions
+
+
+    final static String ID = "id";
+    final static String DATE = "date";
+    final static String DEPARTURE_STATION_ID = "departure_station_id";
+    final static String DEPARTURE_STATION_NAME = "departure_station_name";
+    final static String DEPARTURE_TIME = "departure_time";
+    final static String ARRIVAL_STATION_ID = "arrival_station_id";
+    final static String ARRIVAL_STATION_NAME = "arrival_station_name";
+    final static String ARRIVAL_TIME = "arrival_time";
+    final static String TRAIN_ROUTE_ID = "train_route_id";
+    final static String TRAIN_NAME = "train_name";
+    final static String ROUTE_START_STATION_NAME = "route_start_station_name";
+    final static String ROUTE_END_STATION_NAME = "route_end_station_name";
+    final static String TABLE_NAME = "timetable";
+
+}
