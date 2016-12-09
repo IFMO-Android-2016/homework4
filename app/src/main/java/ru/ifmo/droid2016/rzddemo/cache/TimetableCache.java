93d5b2a1106104d8659e8f74245119b71d3b2ca7
Egor Safronov <safronov.egor.31@gmail.com>
2016-12-09 06:49:54 +0300

    Homework#4 done

--- app/src/main/java/ru/ifmo/droid2016/rzddemo/cache/TimetableCache.java
+++ app/src/main/java/ru/ifmo/droid2016/rzddemo/cache/TimetableCache.java
@@ -1,16 +1,21 @@
 package ru.ifmo.droid2016.rzddemo.cache;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.support.annotation.AnyThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.WorkerThread;
 
 import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 import ru.ifmo.droid2016.rzddemo.model.TimetableEntry;
+import ru.ifmo.droid2016.rzddemo.utils.TimeUtils;
 
 import static ru.ifmo.droid2016.rzddemo.Constants.LOG_DATE_FORMAT;
 
@@ -65,10 +70,84 @@ public class TimetableCache {
                                     @NonNull String toStationId,
                                     @NonNull Calendar dateMsk)
             throws FileNotFoundException {
-        // TODO: ДЗ - реализовать кэш на основе базы данных SQLite с учетом версии модели данных
-        throw new FileNotFoundException("No data in timetable cache for: fromStationId="
-                + fromStationId + ", toStationId=" + toStationId
-                + ", dateMsk=" + LOG_DATE_FORMAT.format(dateMsk.getTime()));
+        SQLiteDatabase db = TimetableDatabaseHelper.getInstance(context, version).getReadableDatabase();
+        String[] projection;
+        if (version == DataSchemeVersion.V1) {
+            projection = new String[] {TimetableCacheContract.Timetable.DEPARTURE_STATION_ID,
+                    TimetableCacheContract.Timetable.DEPARTURE_STATION_NAME,
+                    TimetableCacheContract.Timetable.DEPARTURE_TIME,
+                    TimetableCacheContract.Timetable.ARRIVAL_STATION_ID,
+                    TimetableCacheContract.Timetable.ARRIVAL_STATION_NAME,
+                    TimetableCacheContract.Timetable.ARRIVAL_TIME,
+                    TimetableCacheContract.Timetable.TRAIN_ROUTE_ID,
+                    TimetableCacheContract.Timetable.ROUTE_START_STATION_NAME,
+                    TimetableCacheContract.Timetable.ROUTE_END_STATION_NAME};
+        } else {
+            projection = new String[] {TimetableCacheContract.Timetable.DEPARTURE_STATION_ID,
+                    TimetableCacheContract.Timetable.DEPARTURE_STATION_NAME,
+                    TimetableCacheContract.Timetable.DEPARTURE_TIME,
+                    TimetableCacheContract.Timetable.ARRIVAL_STATION_ID,
+                    TimetableCacheContract.Timetable.ARRIVAL_STATION_NAME,
+                    TimetableCacheContract.Timetable.ARRIVAL_TIME,
+                    TimetableCacheContract.Timetable.TRAIN_ROUTE_ID,
+                    TimetableCacheContract.Timetable.ROUTE_START_STATION_NAME,
+                    TimetableCacheContract.Timetable.ROUTE_END_STATION_NAME,
+                    TimetableCacheContract.Timetable.TRAIN_NAME};
+        }
+
+        List<TimetableEntry> timetable = new ArrayList<>();
+
+        String selection = TimetableCacheContract.Timetable.DEPARTURE_STATION_ID + "=? AND "
+                + TimetableCacheContract.Timetable.ARRIVAL_STATION_ID + "=? AND "
+                + TimetableCacheContract.Timetable.DEPARTURE_DATE + "=?";
+
+        Cursor cursor = null;
+        try {
+            cursor = db.query(TimetableCacheContract.Timetable.TABLE, projection, selection,
+                    new String[] {fromStationId, toStationId, Long.toString(getDay(dateMsk))},
+                    null, null, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                for (; !cursor.isAfterLast(); cursor.moveToNext()) {
+                    String departureStationId = cursor.getString(0);
+                    String departureStationName = cursor.getString(1);
+                    Calendar departureTime = getCalendar(cursor.getLong(2));
+                    String arrivalStationId = cursor.getString(3);
+                    String arrivalStationName = cursor.getString(4);
+                    Calendar arrivalTime = getCalendar(cursor.getLong(5));
+                    String trainRouteId = cursor.getString(6);
+                    String routeStartStationName = cursor.getString(7);
+                    String routeEndStationName = cursor.getString(8);
+                    String trainName;
+                    if (version == DataSchemeVersion.V2) {
+                        trainName = cursor.getString(9);
+                    } else {
+                        trainName = null;
+                    }
+                    TimetableEntry entry = new TimetableEntry(departureStationId, departureStationName, departureTime, arrivalStationId, arrivalStationName, arrivalTime, trainRouteId, trainName, routeStartStationName, routeEndStationName);
+                    timetable.add(entry);
+                }
+            } else {
+                throw new FileNotFoundException("No data in timetable cache for: fromStationId="
+                        + fromStationId + ", toStationId=" + toStationId
+                        + ", dateMsk=" + LOG_DATE_FORMAT.format(dateMsk.getTime()));
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+
+        return timetable;
+    }
+
+    private Calendar getCalendar(long aLong) {
+        Calendar calendar = Calendar.getInstance(TimeUtils.getMskTimeZone());
+        calendar.setTimeInMillis(aLong);
+        return calendar;
+    }
+
+    private long getDay(Calendar date) {
+        return date.get(Calendar.YEAR) * 1000 + date.get(Calendar.DAY_OF_YEAR);
     }
 
     @WorkerThread
@@ -76,6 +155,51 @@ public class TimetableCache {
                     @NonNull String toStationId,
                     @NonNull Calendar dateMsk,
                     @NonNull List<TimetableEntry> timetable) {
-        // TODO: ДЗ - реализовать кэш на основе базы данных SQLite с учетом версии модели данных
+        SQLiteDatabase db = TimetableDatabaseHelper.getInstance(context, version).getWritableDatabase();
+        String toInsert = "INSERT INTO " + TimetableCacheContract.Timetable.TABLE + " ("
+                + TimetableCacheContract.Timetable.DEPARTURE_DATE + ", "
+                + TimetableCacheContract.Timetable.DEPARTURE_STATION_ID + ", "
+                + TimetableCacheContract.Timetable.DEPARTURE_STATION_NAME + ", "
+                + TimetableCacheContract.Timetable.DEPARTURE_TIME + ", "
+                + TimetableCacheContract.Timetable.ARRIVAL_STATION_ID + ", "
+                + TimetableCacheContract.Timetable.ARRIVAL_STATION_NAME + ", "
+                + TimetableCacheContract.Timetable.ARRIVAL_TIME + ", "
+                + TimetableCacheContract.Timetable.TRAIN_ROUTE_ID + ", "
+                + TimetableCacheContract.Timetable.ROUTE_START_STATION_NAME + ", "
+                + TimetableCacheContract.Timetable.ROUTE_END_STATION_NAME;
+        if (version == DataSchemeVersion.V1) {
+            toInsert += ") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        } else {
+            toInsert += ", " + TimetableCacheContract.Timetable.TRAIN_NAME + ") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        }
+
+        db.beginTransaction();
+        try {
+            SQLiteStatement insertStatement = db.compileStatement(toInsert);
+            for (TimetableEntry entry: timetable) {
+                insertStatement.bindLong(1, getDay(dateMsk));
+                insertStatement.bindString(2, entry.departureStationId);
+                insertStatement.bindString(3, entry.departureStationName);
+                insertStatement.bindLong(4, entry.departureTime.getTimeInMillis());
+                insertStatement.bindString(5, entry.arrivalStationId);
+                insertStatement.bindString(6, entry.arrivalStationName);
+                insertStatement.bindLong(7, entry.arrivalTime.getTimeInMillis());
+                insertStatement.bindString(8, entry.trainRouteId);
+                insertStatement.bindString(9, entry.routeStartStationName);
+                insertStatement.bindString(10, entry.routeEndStationName);
+                if (version == DataSchemeVersion.V2) {
+                    if (entry.trainName == null) {
+                        insertStatement.bindNull(11);
+                    } else {
+                        insertStatement.bindString(11, entry.trainName);
+                    }
+                }
+                insertStatement.executeInsert();
+            }
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+
     }
 }
