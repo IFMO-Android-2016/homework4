f47511d10582c4748498235c9037c92e82125669
OlegKuro <hayatov@rain.ifmo.ru>
2016-12-16 18:41:31 +0500

    rdy

---
+++ app/src/main/java/ru/ifmo/droid2016/rzddemo/cache/DBHelper.java
@@ -0,0 +1,134 @@
+package ru.ifmo.droid2016.rzddemo.cache;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.util.Calendar;
+import java.util.HashMap;
+
+/**
+ * Created by Елена on 11.12.16.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String TABLE_NAME = "RZD_CASH";
+    public static final String DEPARTURE_STATION_ID = "DEPSTID";
+    public static final String DEPARTURE_STATION_NAME = "DEPSTNAME";
+    public static final String DEPARTURE_TIME = "DEPTIME";
+    public static final String ARRIVAL_STATION_ID = "ARRSTID";
+    public static final String ARRIVAL_STATION_NAME = "ARRSTNAME";
+    public static final String ARRIVAL_TIME = "ARRTIME";
+    public static final String TRAIN_ROUTE_ID = "TRAINROUTEID";
+    public static final String TRAIN_NAME = "TRAINNAME";
+    public static final String ROUTE_START_STATION_NAME = "ROUTESTARTSTNAME";
+    public static final String ROUTE_END_STATION_NAME = "ROUTEENDSTNAME";
+    public static final String DAY = "DAY";
+    public static final HashMap<Integer, DBHelper> instances = new HashMap<>(2);
+    public void onCreate(SQLiteDatabase db) {
+        db.beginTransaction();
+        try {
+            createTable(db, db.getVersion());
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+    }
+    public static DBHelper getInstance(@DataSchemeVersion int version) {
+        return instances.get(version);
+    }
+
+    public static DBHelper getOrCreateInstance(Context context, @DataSchemeVersion int version) {
+        DBHelper ret;
+        synchronized (instances) {
+            ret = instances.get(version);
+            if (ret == null) ret = createInstance(context, version);
+        }
+        return ret;
+    }
+    public static DBHelper createInstance(Context context, int version, DatabaseErrorHandler errorHandler) {
+        synchronized (instances) {
+            if (instances.get(version) != null) throw new IllegalStateException("instance already here");
+            DBHelper ret = new DBHelper(context, version, errorHandler);
+            instances.put(version, ret);
+            return ret;
+        }
+    }
+    public static DBHelper createInstance(Context context, @DataSchemeVersion int version) {
+        return createInstance(context, version, null);
+    }
+
+    private DBHelper(Context context, @DataSchemeVersion int version) {
+        this(context, version, null);
+    }
+
+    public DBHelper(Context context, @DataSchemeVersion int version, DatabaseErrorHandler errorHandler) {
+        super(context, "ruIfmoDroid2016RzdDemo", null, version, errorHandler);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (newVersion != 1) throw new RuntimeException("Wrong DB version: " + newVersion);
+        db.beginTransaction();
+        try {
+            db.execSQL("ALTER TABLE " + TABLE_NAME + " RENAME TO TMP");
+            createTable(db, 1);
+            Cursor c = null;
+            try {
+                c = db.rawQuery("SELECT * FROM TMP", new String[0]);
+                for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+                    ContentValues cv = new ContentValues(c.getColumnCount());
+                    cv.put(DAY, c.getLong(c.getColumnIndex(DAY)));
+                    cv.put(DEPARTURE_STATION_ID, c.getLong(c.getColumnIndex(DEPARTURE_STATION_ID)));
+                    cv.put(DEPARTURE_STATION_NAME, c.getString(c.getColumnIndex(DEPARTURE_STATION_NAME)));
+                    cv.put(DEPARTURE_TIME, c.getString(c.getColumnIndex(DEPARTURE_TIME)));
+                    cv.put(ARRIVAL_STATION_ID, c.getLong(c.getColumnIndex(ARRIVAL_STATION_ID)));
+                    cv.put(ARRIVAL_STATION_NAME, c.getString(c.getColumnIndex(ARRIVAL_STATION_NAME)));
+                    cv.put(ARRIVAL_TIME, c.getString(c.getColumnIndex(ARRIVAL_TIME)));
+                    cv.put(TRAIN_ROUTE_ID, c.getLong(c.getColumnIndex(TRAIN_ROUTE_ID)));
+                    cv.put(ROUTE_START_STATION_NAME, c.getString(c.getColumnIndex(ROUTE_START_STATION_NAME)));
+                    cv.put(ROUTE_END_STATION_NAME, c.getString(c.getColumnIndex(ROUTE_END_STATION_NAME)));
+                    db.insert(TABLE_NAME, null, cv);
+                }
+            } finally {
+                if (c != null) c.close();
+            }
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion == newVersion) return;
+        if (newVersion != 2) throw new RuntimeException("DB version is incorrect: " + newVersion);
+        db.beginTransaction();
+        try {
+            db.execSQL("ALTER TABLE " + TABLE_NAME + " ADD COLUMN " + TRAIN_NAME + " TEXT;");
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+    }
+    private void createTable(SQLiteDatabase db, int version) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " ("
+                + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + DEPARTURE_STATION_ID + " INTEGER, "
+                + DEPARTURE_STATION_NAME + " TEXT, "
+                + DEPARTURE_TIME + " INTEGER, "
+                + ARRIVAL_STATION_ID + " INTEGER, "
+                + ARRIVAL_STATION_NAME + " TEXT, "
+                + ARRIVAL_TIME + " INTEGER, "
+                + TRAIN_ROUTE_ID + " INTEGER, "
+                + (version == 2 ? TRAIN_NAME + " TEXT, " : "")
+                + ROUTE_START_STATION_NAME + " TEXT, "
+                + ROUTE_END_STATION_NAME + " TEXT, "
+                + DAY + " INTEGER);");
+    }
+    static long currentDay(Calendar calendar) {
+        return calendar.get(Calendar.DAY_OF_YEAR) + calendar.get(Calendar.YEAR) * 366; //probably 365
+    }
+}
--- app/src/main/java/ru/ifmo/droid2016/rzddemo/cache/TimetableCache.java
+++ app/src/main/java/ru/ifmo/droid2016/rzddemo/cache/TimetableCache.java
@@ -1,15 +1,19 @@
 package ru.ifmo.droid2016.rzddemo.cache;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.provider.BaseColumns;
 import android.support.annotation.AnyThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.WorkerThread;
 
 import java.io.FileNotFoundException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
+import android.util.Log;
 import ru.ifmo.droid2016.rzddemo.model.TimetableEntry;
 
 import static ru.ifmo.droid2016.rzddemo.Constants.LOG_DATE_FORMAT;
@@ -29,10 +33,12 @@ public class TimetableCache {
     private final Context context;
 
     /**
-     * Версия модели данных, с которой работает кэщ.
+     * Версия модели данных, с которой работает кэш.
      */
     @DataSchemeVersion
     private final int version;
+
+    private SQLiteStatement insertStatement = null;
 
     /**
      * Создает экземпляр кэша с указанной версией модели данных.
@@ -65,10 +71,59 @@ public class TimetableCache {
                                     @NonNull String toStationId,
                                     @NonNull Calendar dateMsk)
             throws FileNotFoundException {
-        // TODO: ДЗ - реализовать кэш на основе базы данных SQLite с учетом версии модели данных
-        throw new FileNotFoundException("No data in timetable cache for: fromStationId="
+        final DBHelper helper = DBHelper.getOrCreateInstance(context, version);
+        final SQLiteDatabase db = helper.getReadableDatabase();
+        List<TimetableEntry> timetableEntryList = new LinkedList<>();
+        Cursor c = null;
+        long day = DBHelper.currentDay(dateMsk);
+        Log.wtf("RZDDBG", Long.toString(day));
+        db.beginTransaction();
+        try {
+            c = db.rawQuery("SELECT * FROM " + DBHelper.TABLE_NAME + " WHERE " +
+                    DBHelper.DEPARTURE_STATION_ID + " = " + fromStationId + " AND " +
+                    DBHelper.ARRIVAL_STATION_ID + " = " + toStationId + " AND " +
+                    DBHelper.DAY + " = " + day, new String[0]);
+            int ciDep = c.getColumnIndex(DBHelper.DEPARTURE_TIME);
+            int ciArr = c.getColumnIndex(DBHelper.ARRIVAL_TIME);
+            int ciDSN = c.getColumnIndex(DBHelper.DEPARTURE_STATION_NAME);
+            int ciASN = c.getColumnIndex(DBHelper.ARRIVAL_STATION_NAME);
+            int ciTRI = c.getColumnIndex(DBHelper.TRAIN_ROUTE_ID);
+            int ciTI  = c.getColumnIndex(DBHelper.TRAIN_NAME);
+            int ciRSN = c.getColumnIndex(DBHelper.ROUTE_START_STATION_NAME);
+            int ciREN = c.getColumnIndex(DBHelper.ROUTE_END_STATION_NAME);
+            Log.d("RZDDB", String.valueOf(c.getPosition()));
+            Log.d("RZDDB", String.valueOf(c.getCount()));
+            for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+                Log.d("RZDDB", String.valueOf(c.getPosition()));
+                Log.d("RZDDB", String.valueOf(c.getLong(c.getColumnIndex(BaseColumns._ID))));
+                Calendar depTime = new GregorianCalendar();
+                Calendar arrTime = new GregorianCalendar();
+                depTime.setTimeInMillis(c.getLong(ciDep));
+                arrTime.setTimeInMillis(c.getLong(ciArr));
+                timetableEntryList.add(new TimetableEntry(
+                        fromStationId,
+                        c.getString(ciDSN),
+                        depTime,
+                        toStationId,
+                        c.getString(ciASN),
+                        arrTime,
+                        c.getString(ciTRI),
+                        (version == DataSchemeVersion.V2 ? c.getString(ciTI) : null),
+                        c.getString(ciRSN),
+                        c.getString(ciREN)));
+            }
+            db.setTransactionSuccessful();
+        } catch (Throwable e) {
+            Log.e("RZDDB", e.getMessage());
+            e.printStackTrace();
+        } finally {
+            if (c != null) c.close();
+            db.endTransaction();
+        }
+        if (timetableEntryList.size() == 0) throw new FileNotFoundException("No data in timetable cache for: fromStationId="
                 + fromStationId + ", toStationId=" + toStationId
                 + ", dateMsk=" + LOG_DATE_FORMAT.format(dateMsk.getTime()));
+        return timetableEntryList;
     }
 
     @WorkerThread
@@ -76,6 +131,54 @@ public class TimetableCache {
                     @NonNull String toStationId,
                     @NonNull Calendar dateMsk,
                     @NonNull List<TimetableEntry> timetable) {
-        // TODO: ДЗ - реализовать кэш на основе базы данных SQLite с учетом версии модели данных
+        SQLiteDatabase db = DBHelper.getOrCreateInstance(context, version).getWritableDatabase();
+        long day = DBHelper.currentDay(dateMsk);
+        db.beginTransaction();
+        Log.wtf("RZDDBP", Long.toString(day));
+        try {
+            if (insertStatement == null) insertStatement = db.compileStatement("insert into "
+                    + DBHelper.TABLE_NAME
+                    + " ("
+                    + DBHelper.DAY + ", "
+                    + DBHelper.DEPARTURE_STATION_ID + ", "
+                    + DBHelper.DEPARTURE_STATION_NAME + ", "
+                    + DBHelper.DEPARTURE_TIME + ", "
+                    + DBHelper.ARRIVAL_STATION_ID + ", "
+                    + DBHelper.ARRIVAL_STATION_NAME + ", "
+                    + DBHelper.ARRIVAL_TIME + ", "
+                    + DBHelper.TRAIN_ROUTE_ID + ", "
+                    + DBHelper.ROUTE_START_STATION_NAME + ", "
+                    + DBHelper.ROUTE_END_STATION_NAME
+                    + (version == DataSchemeVersion.V2 ? ", " + DBHelper.TRAIN_NAME : "") + ")"
+                    + " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?" + (version == DataSchemeVersion.V2 ? ", ?)" : ")"));
+            insertStatement.bindLong(1, day);
+            insertStatement.bindString(2, fromStationId);
+            insertStatement.bindString(5, toStationId);
+            Log.d("RZDDBV", String.valueOf(version));
+            for (TimetableEntry e : timetable) {
+                insertStatement.bindString(3, e.departureStationName);
+                insertStatement.bindLong(4, e.departureTime.getTimeInMillis());
+                insertStatement.bindString(6, e.arrivalStationName);
+                insertStatement.bindLong(7, e.arrivalTime.getTimeInMillis());
+                insertStatement.bindString(8, e.trainRouteId);
+                insertStatement.bindString(9, e.routeStartStationName);
+                insertStatement.bindString(10, e.routeEndStationName);
+                if (version == DataSchemeVersion.V2) {
+                    if (e.trainName == null) {
+                        insertStatement.bindNull(11);
+                    } else {
+                        insertStatement.bindString(11, e.trainName);
+                    }
+                }
+                insertStatement.executeInsert();
+            }
+            insertStatement.clearBindings();
+            db.setTransactionSuccessful();
+        } catch (Throwable e) {
+            Log.d("RZDDB", e.getMessage());
+            e.printStackTrace();
+        } finally {
+            db.endTransaction();
+        }
     }
 }
